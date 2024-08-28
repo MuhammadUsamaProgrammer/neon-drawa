3a141a6ee6d75482474749883b4464d361fdbf84
Muhammad Usama <usama.flutterdev@gmail.com>
2024-08-28 12:01:46 +0500

    First push

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:speedometer/neon_draw.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Neon Trail',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      home: NeonTrailScreen(),
+    );
+  }
+}
